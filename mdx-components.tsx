cb6c9e95ade7ea90ec2ef422be7f44afbbba1349
Isaac Vargas <hello@isaacvargas.dev>
2026-07-26 16:17:51 -0700

    feat: add details to con-carino content page

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,6 +4,9 @@ const components: MDXComponents = {
   h1: ({ children }) => (
     <h1 className="text-3xl font-semibold text-accent">{children}</h1>
   ),
+  h2: ({ children }) => (
+    <h1 className="text-xl font-semibold text-accent">{children}</h1>
+  ),
 } satisfies MDXComponents;
 
 
